95088ffef7c304c53fd16f7099db412bb74cf44d
Nicolai K <eaaniklo@students.eaaa.dk>
2017-11-06 12:05:11 +0100

    ++

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi Git");
         System.out.println(calc());
+        String s = calc();
+        stuff(s);
+        System.out.println(s);
     }
-    public static String calc(){
+    private static String calc(){
         return "Stuff";
     }
+private static void stuff(String stuff){
+        stuff = "Dette her " + stuff;
+}
+
 }

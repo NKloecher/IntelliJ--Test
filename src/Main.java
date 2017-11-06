b7238f4fd6b3f44b4c55f3309825e39d2cfcf07d
Nicolai K <eaaniklo@students.eaaa.dk>
2017-11-06 11:57:57 +0100

    ++

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi Git");
+        System.out.println(calc());
+    }
+    public static String calc(){
+        return "Stuff";
     }
 }
